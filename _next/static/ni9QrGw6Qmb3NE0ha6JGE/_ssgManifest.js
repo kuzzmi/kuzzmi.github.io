96f7180b0c7dc52da8266eb9fea9817f5f3b1716
Igor Kuzmenko <igor@kuzzmi.com>
2022-04-10 13:27:52 +0300

    Updates

--- _next/static/ni9QrGw6Qmb3NE0ha6JGE/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]\u002Fabout","\u002Fthoughts\u002F[slug]","\u002Fblog\u002F[slug]","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fthoughts","\u002F[locale]\u002Fblog","\u002F[locale]","\u002F[locale]\u002Fthoughts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
